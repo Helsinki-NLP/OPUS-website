71166c38b909091cca214a9a594263d205530e65
Joerg Tiedemann <jorg.tiedemann@helsinki.fi>
2023-09-30 23:10:52 +0300

    EOPC added

---
+++ public_html/EOPC-v2022.php
@@ -0,0 +1,103 @@
+<?php include("count.php"); ?>
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	 "http://www.w3.org/TR/html4/loose.dtd">
+<html><head><title>EOPC</title>
+<link rev="made" href="mailto:Joerg%20Tiedemann">
+
+<meta name="robots" content="NOFOLLOW">
+<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+<div class="header"><?php include("header.php"); ?></div><h1>EOPC v2022</h1>This corpus has been collected within the framework of “Estonian Open Parallel Corpus”. The purpose of the project is to create a significant amount of language resources to improve machine translation systems.<p>7 languages, 6 bitexts<br>total number of files: 30<br>total number of tokens: 90.40M<br>total number of sentence fragments: 4.06M<br><p>Please <a href="http://opus.lingfil.uu.se/LREC2012.txt">cite the following article</a> if you use the OPUS packages and downloads in your own work:<br/> J. Tiedemann, 2012, <a href="http://www.lrec-conf.org/proceedings/lrec2012/pdf/463_Paper.pdf"><i>Parallel Data, Tools and Interfaces in OPUS.</i></a> In Proceedings of the 8th International Conference on Language Resources and Evaluation (LREC 2012)<br/><h3>Download</h3><p>Below you can download data files for all language pairs in different formats and with different kind of annotation (if available). You can click on the various links as explained below. In addition to the files shown on this webpage, OPUS also provides pre-compiled word alignments and phrase tables, bilingual dictionaries, frequency counts, and these files can be found through the <a href="/index.php">resources search form on the top-level website of OPUS</a>.</p><p><b>License:</b> CC-BY-SA<br/></p><h3>Release history:</h3><p><ul><li><a href="EOPC-v2022.php">EOPC v2022</a></li></ul></p><table><tr><td>Bottom-left triangle: download files<ul><li><i>ces</i> = sentence alignments in XCES format</li> <li><i>leftmost column language IDs</i> = tokenized corpus files in XML</li> <li>TMX and plain text files (Moses): see "Statistics" below</li> <li><i>lower row language IDs</i> = parsed corpus files (if they exist)</li></ul></td><td>Upper-right triangle: sample files <ul><li><i>view</i> = bilingual XML file samples</li> <li><i>upper row language IDs</i> = monolingual XML file samples</li> <li><i>rightmost column language IDs</i> = untokenized corpus files</li></ul></td></tr></table><p><div class="sample"><table border="0" cellpadding="0">
+<tr>
+<th></th>
+<th><a rel="nofollow" href="EOPC/v2022/de_sample.html">de</a></th>
+<th><a rel="nofollow" href="EOPC/v2022/en_sample.html">en</a></th>
+<th><a rel="nofollow" href="EOPC/v2022/et_sample.html">et</a></th>
+<th><a rel="nofollow" href="EOPC/v2022/fi_sample.html">fi</a></th>
+<th><a rel="nofollow" href="EOPC/v2022/fr_sample.html">fr</a></th>
+<th><a rel="nofollow" href="EOPC/v2022/ru_sample.html">ru</a></th>
+<th><a rel="nofollow" href="EOPC/v2022/sv_sample.html">sv</a></th>
+<th></th></tr>
+<tr><th><a rel="nofollow" href="download.php?f=EOPC/v2022/xml/de.zip">de</a></th>
+<th></th>
+<td></td><td><a rel="nofollow" title="German-Estonian (sample file)" href="EOPC/v2022/de-et_sample.html">view</a></td><td></td><td></td><td></td><td></td><th><a rel="nofollow" href="download.php?f=EOPC/v2022/raw/de.zip">de</a></th></tr>
+<tr><th><a rel="nofollow" href="download.php?f=EOPC/v2022/xml/en.zip">en</a></th>
+<td></td>
+<th></th>
+<td><a rel="nofollow" title="English-Estonian (sample file)" href="EOPC/v2022/en-et_sample.html">view</a></td><td></td><td></td><td></td><td></td><th><a rel="nofollow" href="download.php?f=EOPC/v2022/raw/en.zip">en</a></th></tr>
+<tr><th><a rel="nofollow" href="download.php?f=EOPC/v2022/xml/et.zip">et</a></th>
+<td><a rel="nofollow" title="sentence alignments for 'Estonian-German' (4 aligned documents, 0.4M links, 19.7M tokens)" href="download.php?f=EOPC/v2022/xml/de-et.xml.gz">ces</a></td>
+<td><a rel="nofollow" title="sentence alignments for 'Estonian-English' (4 aligned documents, 0.5M links, 20.8M tokens)" href="download.php?f=EOPC/v2022/xml/en-et.xml.gz">ces</a></td>
+<th></th>
+<td><a rel="nofollow" title="Estonian-Finnish (sample file)" href="EOPC/v2022/et-fi_sample.html">view</a></td><td><a rel="nofollow" title="Estonian-French (sample file)" href="EOPC/v2022/et-fr_sample.html">view</a></td><td><a rel="nofollow" title="Estonian-Russian (sample file)" href="EOPC/v2022/et-ru_sample.html">view</a></td><td><a rel="nofollow" title="Estonian-Swedish (sample file)" href="EOPC/v2022/et-sv_sample.html">view</a></td><th><a rel="nofollow" href="download.php?f=EOPC/v2022/raw/et.zip">et</a></th></tr>
+<tr><th><a rel="nofollow" href="download.php?f=EOPC/v2022/xml/fi.zip">fi</a></th>
+<td></td>
+<td></td>
+<td><a rel="nofollow" title="sentence alignments for 'Finnish-Estonian' (0.4M links, 16.5M tokens)" href="download.php?f=EOPC/v2022/xml/et-fi.xml.gz">ces</a></td>
+<th></th>
+<td></td><td></td><td></td><th><a rel="nofollow" href="download.php?f=EOPC/v2022/raw/fi.zip">fi</a></th></tr>
+<tr><th><a rel="nofollow" href="download.php?f=EOPC/v2022/xml/fr.zip">fr</a></th>
+<td></td>
+<td></td>
+<td><a rel="nofollow" title="sentence alignments for 'French-Estonian' (3 aligned documents, 90.4k links, 4.7M tokens)" href="download.php?f=EOPC/v2022/xml/et-fr.xml.gz">ces</a></td>
+<td></td>
+<th></th>
+<td></td><td></td><th><a rel="nofollow" href="download.php?f=EOPC/v2022/raw/fr.zip">fr</a></th></tr>
+<tr><th><a rel="nofollow" href="download.php?f=EOPC/v2022/xml/ru.zip">ru</a></th>
+<td></td>
+<td></td>
+<td><a rel="nofollow" title="sentence alignments for 'Russian-Estonian' (2 aligned documents, 0.3M links, 10.8M tokens)" href="download.php?f=EOPC/v2022/xml/et-ru.xml.gz">ces</a></td>
+<td></td>
+<td></td>
+<th></th>
+<td></td><th><a rel="nofollow" href="download.php?f=EOPC/v2022/raw/ru.zip">ru</a></th></tr>
+<tr><th><a rel="nofollow" href="download.php?f=EOPC/v2022/xml/sv.zip">sv</a></th>
+<td></td>
+<td></td>
+<td><a rel="nofollow" title="sentence alignments for 'Swedish-Estonian' (0.4M links, 17.9M tokens)" href="download.php?f=EOPC/v2022/xml/et-sv.xml.gz">ces</a></td>
+<td></td>
+<td></td>
+<td></td>
+<th></th>
+<th><a rel="nofollow" href="download.php?f=EOPC/v2022/raw/sv.zip">sv</a></th></tr>
+<tr><th></th>
+<th>de</th>
+<th>en</th>
+<th>et</th>
+<th>fi</th>
+<th>fr</th>
+<th>ru</th>
+<th>sv</th>
+<th></th></tr>
+</table>
+</div><p><h3>Statistics and TMX/Moses Downloads</h3>Number of files, tokens, and sentences per language (including non-parallel ones if they exist)<br>Number of sentence alignment units per language pair<p>Upper-right triangle: download translation memory files (TMX)<br>Bottom-left triangle: download plain text files (MOSES/GIZA++)<br>Language ID's, first row: monolingual plain text files (tokenized)<br>Language ID's, first column: monolingual plain text files (untokenized)<div class="counts"><table><caption></caption> <tr><th>language</th> <th>files</th> <th>tokens</th> <th>sentences</th><th><a rel="nofollow" title='monolingual tokenized de plain text' href="download.php?f=EOPC/v2022/mono/de.tok.gz">de</a>
+</th><th><a rel="nofollow" title='monolingual tokenized en plain text' href="download.php?f=EOPC/v2022/mono/en.tok.gz">en</a>
+</th><th><a rel="nofollow" title='monolingual tokenized et plain text' href="download.php?f=EOPC/v2022/mono/et.tok.gz">et</a>
+</th><th><a rel="nofollow" title='monolingual tokenized fi plain text' href="download.php?f=EOPC/v2022/mono/fi.tok.gz">fi</a>
+</th><th><a rel="nofollow" title='monolingual tokenized fr plain text' href="download.php?f=EOPC/v2022/mono/fr.tok.gz">fr</a>
+</th><th><a rel="nofollow" title='monolingual tokenized ru plain text' href="download.php?f=EOPC/v2022/mono/ru.tok.gz">ru</a>
+</th><th><a rel="nofollow" title='monolingual tokenized sv plain text' href="download.php?f=EOPC/v2022/mono/sv.tok.gz">sv</a>
+</th></tr> <tr><th><a rel="nofollow" title='monolingual untokenized de plain text' href="download.php?f=EOPC/v2022/mono/de.txt.gz">de</a>
+</th><td>4</td> <td>10.9M</td> <td>0.4M</td><td></td><td></td><td bgcolor="#daffc6"><a rel="nofollow" title='German-Estonian (368,753 sentence pairs, 16.60M words) - TMX format' href="download.php?f=EOPC/v2022/tmx/de-et.tmx.gz">0.4M</a>
+</td><td></td><td></td><td></td><td></td></tr> <tr><th><a rel="nofollow" title='monolingual untokenized en plain text' href="download.php?f=EOPC/v2022/mono/en.txt.gz">en</a>
+</th><td>4</td> <td>12.5M</td> <td>0.5M</td><td></td><td></td><td bgcolor="#d9ffc6"><a rel="nofollow" title='English-Estonian (448,561 sentence pairs, 17.72M words) - TMX format' href="download.php?f=EOPC/v2022/tmx/en-et.tmx.gz">0.4M</a>
+</td><td></td><td></td><td></td><td></td></tr> <tr><th><a rel="nofollow" title='monolingual untokenized et plain text' href="download.php?f=EOPC/v2022/mono/et.txt.gz">et</a>
+</th><td>15</td> <td>40.5M</td> <td>2.0M</td><td bgcolor="#daffc6"><a rel="nofollow" title='Estonian-German (371,203 sentence pairs, 16.71M words) - Moses format' href="download.php?f=EOPC/v2022/moses/de-et.txt.zip">0.4M</a>
+</td><td bgcolor="#d9ffc6"><a rel="nofollow" title='Estonian-English (461,669 sentence pairs, 18.20M words) - Moses format' href="download.php?f=EOPC/v2022/moses/en-et.txt.zip">0.5M</a>
+</td><td></td><td bgcolor="#dcffc6"><a rel="nofollow" title='Estonian-Finnish (404,966 sentence pairs, 13.98M words) - TMX format' href="download.php?f=EOPC/v2022/tmx/et-fi.tmx.gz">0.4M</a>
+</td><td bgcolor="#e9ffcb"><a rel="nofollow" title='Estonian-French (90,388 sentence pairs, 3.96M words) - TMX format' href="download.php?f=EOPC/v2022/tmx/et-fr.tmx.gz">90.4k</a>
+</td><td bgcolor="#e0ffc6"><a rel="nofollow" title='Estonian-Russian (313,510 sentence pairs, 9.12M words) - TMX format' href="download.php?f=EOPC/v2022/tmx/et-ru.tmx.gz">0.3M</a>
+</td><td bgcolor="#dbffc6"><a rel="nofollow" title='Estonian-Swedish (372,132 sentence pairs, 15.63M words) - TMX format' href="download.php?f=EOPC/v2022/tmx/et-sv.tmx.gz">0.4M</a>
+</td></tr> <tr><th><a rel="nofollow" title='monolingual untokenized fi plain text' href="download.php?f=EOPC/v2022/mono/fi.txt.gz">fi</a>
+</th><td>1</td> <td>8.2M</td> <td>0.4M</td><td></td><td></td><td bgcolor="#dcffc6"><a rel="nofollow" title='Finnish-Estonian (404,966 sentence pairs, 13.98M words) - Moses format' href="download.php?f=EOPC/v2022/moses/et-fi.txt.zip">0.4M</a>
+</td><td></td><td></td><td></td><td></td></tr> <tr><th><a rel="nofollow" title='monolingual untokenized fr plain text' href="download.php?f=EOPC/v2022/mono/fr.txt.gz">fr</a>
+</th><td>3</td> <td>2.9M</td> <td>90.4k</td><td></td><td></td><td bgcolor="#e9ffcb"><a rel="nofollow" title='French-Estonian (90,395 sentence pairs, 3.96M words) - Moses format' href="download.php?f=EOPC/v2022/moses/et-fr.txt.zip">90.4k</a>
+</td><td></td><td></td><td></td><td></td></tr> <tr><th><a rel="nofollow" title='monolingual untokenized ru plain text' href="download.php?f=EOPC/v2022/mono/ru.txt.gz">ru</a>
+</th><td>2</td> <td>5.8M</td> <td>0.3M</td><td></td><td></td><td bgcolor="#e0ffc6"><a rel="nofollow" title='Russian-Estonian (313,727 sentence pairs, 9.13M words) - Moses format' href="download.php?f=EOPC/v2022/moses/et-ru.txt.zip">0.3M</a>
+</td><td></td><td></td><td></td><td></td></tr> <tr><th><a rel="nofollow" title='monolingual untokenized sv plain text' href="download.php?f=EOPC/v2022/mono/sv.txt.gz">sv</a>
+</th><td>1</td> <td>9.7M</td> <td>0.4M</td><td></td><td></td><td bgcolor="#dbffc6"><a rel="nofollow" title='Swedish-Estonian (372,132 sentence pairs, 15.63M words) - Moses format' href="download.php?f=EOPC/v2022/moses/et-sv.txt.zip">0.4M</a>
+</td><td></td><td></td><td></td><td></td></tr></table></div><p>Note that TMX files only contain unique translation units and, therefore, the number of aligned units is smaller than for the distributions in Moses and XML format. Moses downloads include all non-empty alignment units including duplicates. Token counts for each language also include duplicate sentences and documents.<p><hr><h3>Disclaimer</h3> <ul> <li>We do not own any of the text from which the data has been extracted.</li> <li>We only offer files that we believe we are free to redistribute. If any doubt occurs about the legality of any of our file downloads we will take them off right away after contacting us.</li> </ul> <h3>Notice and take down policy</h3> Notice: Should you consider that our data contains material that is owned by you and should therefore not be reproduced here, please: <ul> <li>Clearly identify yourself, with detailed contact data such as an address, telephone number or email address at which you can be contacted.</li> <li>Clearly identify the copyrighted work claimed to be infringed.</li> <li>Clearly identify the material that is claimed to be infringing and information reasonably sufficient to allow us to locate the material.</li> <li>And contact Joerg Tiedemann at the following email address: jorg.tiedemann at helsinki.fi.</li> </ul> Take down: We will comply to legitimate requests by removing the affected sources from the next release of the corpus.<div class="footer"></div>
+</body>
+</html>
